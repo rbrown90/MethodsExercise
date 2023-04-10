54473e8e07c15db16f8c11d2610700c4f41bb736
Rakin Brown <rakinbrown38@gmail.com>
2023-04-10 15:40:47 -0500

    finished profile configuration

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace MethodsExercise
+﻿using System;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Name: Kevin
+            // Color: Black
+            // Animal: Elephant
+            // Band: Earth,Wing, and Fire
+
+            Console.WriteLine("Hello, What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("$Hello {userName}! Do you mind if I ask you favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("$I love {color} also. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("{animal} are amazing! What is your favorite band?");
+            var band = Console.ReadLine();
+
+        
+
+            Console.WriteLine($"Name; {userName}");
+            Console.WriteLine($"Favorite Color; {color}");
+            Console.WriteLine($"Favorite Animal; {animal}");
+            Console.WriteLine($"Favorite Band; {band}");
         }
     }
 }
